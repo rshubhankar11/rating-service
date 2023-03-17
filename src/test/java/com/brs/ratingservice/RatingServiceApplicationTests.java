409867f76c29183f98300edfb268985ae48a775c
rabiroshan shubhankar <rshubhankar11@gmail.com>
2023-03-17 17:38:56 +0530

    added basic stracture of rating servie

---
+++ src/test/java/com/brs/ratingservice/RatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brs.ratingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
